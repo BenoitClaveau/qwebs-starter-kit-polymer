8ab00dc7ef76595866b7aa1d2ec77c1f2dd3cc0e
Benoit Claveau <beny78@gmail.com>
2016-12-20 17:14:08 +0100

    Try to use vulcanize

--- server.js
+++ server.js
@@ -3,19 +3,68 @@
 const fs = require("fs");
 const http = require("http");
 const Qwebs = require("qwebs");
-    
-let qwebs = new Qwebs();
-qwebs.load().then(function() {
-    let $config = qwebs.resolve("$config");
-    http.createServer((request, response) => {
-        return qwebs.invoke(request, response).catch(error => {
-            return response.send({ statusCode: 404, content: error });
+// const Vulcanize = require('vulcanize');
+// const crisper = require('crisper');
+// const path = require("path");
+
+
+class Server extends Qwebs {
+    // load() {
+    //     return super.load().then(() => {
+    //         return this.build("index.html").then(html => {
+    //             return this.asset("index.html").init(html).then(() => {
+    //                 return this.asset("my-view1.html").initFromFile("web/src/my-view1.html").then(() => {
+    //                     console.log("loaded")
+    //                 });
+    //             });
+    //         });
+    //     });
+    // }
+
+    // build(htmlmodule, jsmodule) {
+    //     return new Promise((resolve, reject) => {
+    //         let vulcanize = new Vulcanize({
+    //             abspath: 'web',
+    //             implicitStrip: true,
+    //             stripComments: false
+    //         });
+    //         vulcanize.process(htmlmodule, function(err, html) {
+    //             if (err) reject(error);
+    //             else resolve(html);
+    //             // else {
+    //             //     console.log("crisper")
+    //             //     let out = crisper({
+    //             //         source: html,
+    //             //         jsFileName: "app.js",
+    //             //         scriptInHead: true, // default true
+    //             //         onlySplit: false, // default false
+    //             //         alwaysWriteScript: false //default false
+    //             //     });
+    //             //     resolve({
+    //             //         html: out.html,
+    //             //         js: out.js
+    //             //     });
+    //             // }
+    //         });
+    //     });
+    // };
+
+    start() {
+        console.log("start")
+        return this.load().then(() => {
+            let $config = this.resolve("$config");
+            http.createServer((request, response) => {
+                return this.invoke(request, response).catch(error => {
+                    return response.send(error);
+                });
+            }).listen($config.http.port);
+            console.log("http server created on", $config.http.port);
         });
-    }).listen($config.http.port);
-    console.log("http server created on", $config.http.port);
-}).catch(error => {
-    console.log("FATAL: -----------------------");
-    console.log(error);
-    console.log(error.stack);
-    throw error;
+    }
+};
+
+let server = new Server();
+server.start().catch(error => {
+    console.error(error.message);
+    console.error(error.stack);
 });
