445e01cec1e8f61698161d0ab25e6c94d3438cf3
Benoit Claveau <beny78@gmail.com>
2016-12-23 11:53:11 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,19 @@ var vulcanize = require('gulp-vulcanize');
 var crisper = require('gulp-crisper');
 var jeditor = require("gulp-json-editor");
 var path = require("path");
+var minifyInline = require('gulp-minify-inline');
+var rename = require("gulp-rename");
+var concat = require('gulp-concat');
 
-// gulp.task('vulcanize', function() {
-//   return gulp.src([
-//       'public/index.html',
-//     ])
-//     .pipe(vulcanize({
-//       abspath: path.resolve('public'),
-//       inputUrl: '/index.html',
-//       stripComments: true,
-//       inlineScripts: true,
-//       inlineCss: true
-//     }))
-//     .pipe(crisper())
-//     .pipe(gulp.dest('build'));
-// });
-
-gulp.task('vulcanize', function() {
+gulp.task('vulcanize-index', function() {
   gulp.src('public/index.html')
     .pipe(vulcanize({
+      excludes: [
+            '/bower_components/polymer/polymer.html'
+        ],
+        stripExcludes: [
+            '/bower_components/polymer/polymer.html'
+        ],
       abspath: path.resolve('public'),
       inputUrl: '/index.html',
       stripExcludes: false,
@@ -30,9 +24,12 @@ gulp.task('vulcanize', function() {
       implicitStrip: true,
       stripComments: true
     }))
-    .pipe(crisper())
+    .pipe(minifyInline())
+    //.pipe(crisper())
     .pipe(gulp.dest('build'));
+});
 
+gulp.task('vulcanize-views', function() {
   gulp.src(['public/src/*.html', '!public/src/my-app.html'])
     .pipe(vulcanize({
         excludes: [
@@ -46,17 +43,35 @@ gulp.task('vulcanize', function() {
         implicitStrip: true,
         stripComments: true
     }))
-    .pipe(crisper())
+    .pipe(minifyInline())
+    // .pipe(crisper())
     .pipe(gulp.dest("build/src"));
+});
+
+gulp.task('vulcanize-polymer', function() {
+    return gulp.src([
+        'public/bower_components/polymer/polymer.html',
+        'public/bower_components/polymer/polymer-mini.html',
+        'public/bower_components/polymer/polymer-micro.html'
+    ]) 
+      .pipe(concat('polymer.html'))
+      .pipe(vulcanize({
+          inlineScripts: true,
+          inlineCss: true,
+          implicitStrip: true,
+          stripComments: true
+      }))
+      .pipe(gulp.dest('build/bower_components/polymer'));
+});
+
+gulp.task('vulcanize', ['vulcanize-polymer', 'vulcanize-index', 'vulcanize-views'], function(done) {
+  done();
 });
 
 gulp.task('includes',function(){
   return gulp.src([
       'public/images/*',
-      'public/manifest.json',
-      'public/bower_components/polymer/polymer.html',
-      'public/bower_components/polymer/polymer-mini.html',
-      'public/bower_components/polymer/polymer-micro.html'
+      'public/manifest.json'
   ], { base: 'public' }) 
     .pipe(gulp.dest('build'));
 });
